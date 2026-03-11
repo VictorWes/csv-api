9dfff97407c658e5b27dfb233d7c7f294898313b
Victor Wesley <victor.infowb@gmail.com>
2026-03-11 16:25:59 -0300

    test: configura infraestrutura de testcontainers com postgresql
    - Adiciona dependencias do Testcontainers (JUnit Jupiter e PostgreSQL) com versao 1.19.7
    - Cria application-test.properties configurando o Flyway para os testes de integracao
    - Implementa a classe base AbstractIntegrationTest utilizando @ServiceConnection para gerenciar dinamicamente o banco de dados no Docker
    - Refatora CsvApplicationTests para herdar da nova classe base

---
+++ src/test/java/com/csv/AbstractIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.csv;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Testcontainers
+public class AbstractIntegrationTest {
+
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+
+}
--- src/test/java/com/csv/CsvApplicationTests.java
+++ src/test/java/com/csv/CsvApplicationTests.java
@@ -3,11 +3,15 @@ package com.csv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class CsvApplicationTests {
+class CsvApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
+        System.out.println("Banco esta rodando: " + postgres.isRunning());
+        assertTrue(postgres.isRunning());
 	}
 
 }
